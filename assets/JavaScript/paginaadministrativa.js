bdc485e53b96990aad1e589fb912a9d00220fa84
MariaRita011 <mariaritagomes.564@gmail.com>
2023-05-21 10:48:22 -0300

    testes js

--- assets/JavaScript/paginaadministrativa.js
+++ assets/JavaScript/paginaadministrativa.js
@@ -1,1 +1,27 @@
-function adicionar()
+var arrgeral = [];
+
+
+//Function Adicionar
+function adicionar() {
+
+    var arr = [];
+    //Variáveis
+    let title = document.getElementById("titulo").value;
+    let resum = document.getElementById("resumo").value;
+    let categ = document.getElementById("categoria").value;
+    let author = document.getElementById("autor").value;
+    let date = document.getElementById("data").value;
+    let datausa = date;
+    let databr = datausa.split('-').reverse().join('/');
+    arr.push(title, resum, categ, author, databr);
+    arrgeral.push(arr);
+    console.log(arrgeral);
+    document.getElementById("titulo").value = '';
+    document.getElementById("resumo").value = '';
+    document.getElementById("categoria").value = '';
+    document.getElementById("autor").value = '';
+    document.getElementById("data").value = '';
+    arr = [];
+    console.log(arr);
+
+}
