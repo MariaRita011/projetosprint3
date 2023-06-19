c45f64049a66c8e00d24dd18088ab6398060db23
MariaRita011 <mariaritagomes.564@gmail.com>
2023-06-18 21:14:38 -0300

    comentando js

--- assets/JavaScript/paginaadministrativa.js
+++ assets/JavaScript/paginaadministrativa.js
@@ -4,16 +4,13 @@ let categorias = [];
 let resumos = [];
 let autores = [];
 let datas = [];
+
+//Contador
 i = 0;
 
-const datadehoje = new Date();
-/* const diaatual = date.getDate();
-const mesatual = date.getMonth() + 1; 
-const anoatual = date.getFullYear();
-console.log(diaatual, mesatual, anoatual);
 
-let juntar = anoatual + "/" + mesatual + "/" + diaatual;
-console.log(juntar); */
+//coletando que dia é hoje
+const datadehoje = new Date();
 
 
 //Criando Function Adicionar
@@ -30,19 +27,31 @@ function adicionar() {
 
     //Conventendo a data
     var dbr = dusa.split('-').reverse('').join('/');
+    
+    
+    //Definindo data para comparação
     var dtcompar = dusa.split('-').join('/');
-const dateinput = new Date(dtcompar);
+    const dateinput = new Date(dtcompar);
     console.log(dateinput);
+
 
     //Caso algum cmpo não tenha sido preenchido
     if (t == '' || c == '' || r == '' || a == '' || dusa == ''||t == '' &&c == '' && r == '' && a == '' && dusa == '') {
         document.getElementById("verificacao").innerHTML = "Preencha todos os campos para adicionar!";
     } 
+
+
+    //Caso a data seja menor que a data atual
     else if (dateinput.setHours(20) < datadehoje || dateinput.setHours(20) == datadehoje ) {
         document.getElementById("verificacao").innerHTML = "Digite uma data maior que o dia de hoje!";
     }
+
+
+    //bloco de código
 else {
     
+
+    //limpando o campo de verificação
     document.getElementById("verificacao").innerHTML = "";
     //Adiciondo infos no array
     titulos.push(t);
@@ -50,7 +59,8 @@ else {
     resumos.push(r);
     autores.push(a);
     datas.push(dbr);
-    
+
+
     //fazendo aparecer na tela
     document.getElementById("ntc").innerHTML += `<section class="sec" id="${secunica}" >
     <div id="conts" ><h1 id="titulotodo" >${titulos[i]}</h1>
@@ -62,6 +72,7 @@ else {
     </div>
     </section>`;
     
+
     //Limpando campos
     var t = document.getElementById("titulo").value = '';
     var c = document.getElementById("categoria").value = '';
@@ -69,11 +80,15 @@ else {
     var a = document.getElementById("autor").value = '';
     var dusa = document.getElementById("data").value = '';
     
+
+    //adicionando +1 ao contador
     i++
 }
 
 }
 
+
+//Caso o botao deletar seja apertado
 function delet(secunica) {
     var section = document.getElementById(secunica);
     section.style.display = "none";
